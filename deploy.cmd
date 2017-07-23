9f55fa656d20a5b9916c611fa3f6ade64e0d1bdd
Antti Kivimäki <antti.kivimaki@eatech.fi>
2017-07-23 22:46:10 +0300

    Install all dependencies even when NODE_ENV is production

--- deploy.cmd
+++ deploy.cmd
@@ -107,7 +107,7 @@ call :SelectNodeVersion
 :: 3. Install npm packages
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! install
+  call :ExecuteCmd !NPM_CMD! install --production=false
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
