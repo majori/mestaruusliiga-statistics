1fc6585fe74aa898f31ce40d51e3e191e42f7c2f
Antti Kivimäki <antti.kivimaki@eatech.fi>
2017-09-09 20:05:04 +0300

    Parse dates correctly

--- src/services/scraper/matches.ts
+++ src/services/scraper/matches.ts
@@ -1,5 +1,6 @@
 import { load } from 'cheerio';
 import * as _ from 'lodash';
+import * as moment from 'moment';
 import * as request from 'superagent';
 
 export async function getMatches() {
@@ -13,14 +14,29 @@ export async function getMatches() {
 
   $('.ottelulista tbody tr').each((i, e) => {
     const $$ = $(e);
-    matches.push({
+    const match = {
       home: $$.find('td.col_kotisiisti > a').text(),
       guest: $$.find('td.col_vierassiisti > a').text(),
       meta: {
-        date: $$.find('td.col_pvmsiisti').text(),
-        place: $$.find('td.col_info').text(),
+        place: $$.find('td.col_info').text().slice(5),
+        date: moment(),
       },
-    });
+    };
+
+    const clock = _.split($$.find('td.col_info').text().slice(0, 5), ':', 2);
+    const rawDate = $$.find('td.col_pvmsiisti').text().split('.');
+
+    match.meta.date.date(+rawDate[0]);
+    match.meta.date.month(+rawDate[1]);
+
+    if (rawDate[2]) {
+      match.meta.date.year(+rawDate[2]);
+    }
+
+    match.meta.date.hour(+clock[0]);
+    match.meta.date.minute(+clock[1]);
+
+    matches.push(match);
   });
 
   return matches;
